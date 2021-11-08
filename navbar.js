059daf8a7e577ca980e56e78009203e0c9ebf883
Dipankar Sarkar <87586280+Dipankar-gitworld@users.noreply.github.com>
2021-11-08 19:53:28 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,16 @@
+function navbar(){
+    return `<h1>EatFood</h1>
+    <div id="navbar">
+    <h3><a href="menu.html">menu</a></h3>
+    <div>
+        <h3><a href="signup.html">sign up</a></h3>
+        <h3><a href="login.html">login</a></h3>
+        <h3><a href="cart.html">cart</a></h3>
+    </div>
+</div>`
+
+}
+
+
+
+export default navbar
